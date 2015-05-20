a14fac7bc9f8cc30c69a60896cb20caf53d1561d
Kitananji <kitananjikoga@gmail.com>
2015-05-20 19:38:04 +0200

    Projecte
    + articles/preu a int

---
+++ Projecte/MiniERP/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.Odbc;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MiniERP
+{
+    public partial class frmERP : Form
+    {
+        public const string CONNECTIONSTRING = "Driver={Microsoft Access Driver (*.mdb)};DBQ=minierp.mdb";
+
+        private OdbcConnection cn;
+        private OdbcDataAdapter da;
+        DataSet ds;
+
+        //Construct
+        public frmERP()
+        {
+            cn = new OdbcConnection(CONNECTIONSTRING);
+            InitializeComponent();
+        }
+
+        //Events
+        private void frmERP_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exportarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void valoracióStockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void articlesToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ImportarArticles();
+        }
+
+        private void proveïdorsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ImportarProveidors();
+        }
+
+        //Methods
+        private void ImportarArticles()
+        {
+            bool esValid;
+            string codi = "";
+            string descripcio = "";
+            int stock;
+            int preu;
+            string temp;
+            XmlDocument xml;
+            XmlNodeList xnList;
+            esValid = ValidateXML("articles.xml", "articles.xsd");
+            if (esValid)
+            {
+                xml = new XmlDocument();
+                xml.Load("articles.xml");
+                xnList = xml.SelectNodes("/articles/article");
+                cn.Open();
+                OdbcCommand cmd = new OdbcCommand();
+                cmd.Connection = cn;
+
+                foreach (XmlNode xn in xnList)
+                {
+                    codi = xn["codi"].InnerText;
+                    descripcio = xn["descripcio"].InnerText;
+                    stock = Convert.ToInt32(xn["estoc"].InnerText);
+                    temp = xn["preu"].InnerText;
+                    temp = temp.Replace('.', ',');
+                    preu = (int)Convert.ToDouble(temp);
+                    cmd.CommandText = "INSERT INTO article VALUES ('" + codi + "','" + descripcio + "'," + stock + "," + preu + ");";
+                    cmd.ExecuteNonQuery();
+                }
+
+                cn.Close();
+            }
+            else MessageBox.Show("FITXER XML D'ARTICLES NO VÀLID", "Error de validació");
+        }
+
+        private void ImportarProveidors()
+        {
+
+        }
+
+        private bool ValidateXML(string xmlFile, string xsdFile)
+        {
+
+            bool isValid = false;
+            XmlReaderSettings settings = new XmlReaderSettings();
+            try
+            {
+                settings.Schemas.Add(null, xsdFile);
+                settings.ValidationType = ValidationType.Schema;
+                XmlDocument document = new XmlDocument();
+                document.Load(xmlFile);
+                XmlReader rdr = XmlReader.Create(new StringReader(document.InnerXml), settings);
+
+                while (rdr.Read())
+                {
+
+                }
+                isValid = true;
+            }
+            catch { }
+            return isValid;
+        }
+
+    }
+}
+
